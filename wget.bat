b4fd26d59ac35ad91e46c293702ab22243a93088
Ben Aveling <ben.aveling@gmail.com>
2021-08-09 20:51:40 +1000

    wget.bat now displays what it retrieved

--- wget.bat
+++ wget.bat
@@ -1,3 +1,4 @@
-rem This assumes you have a wget.exe, or equivalent.
-rem TODO: work out how to do the same thing using Invoke-WebRequest
+@rem This assumes you have a wget.exe, or equivalent.
+@rem TODO: work out how to do the same thing using Invoke-WebRequest
 wget.exe --no-check-certificate https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv
+dir confirmed_cases_table1_location.csv*
